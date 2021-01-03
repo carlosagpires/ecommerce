45c16ff610e3b95653c1039e5b284def0834e798
Carlos Pires <carlos.alberto.pires@sapo.pt>
2021-01-03 01:58:54 +0000

    Categorias no site

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -37,6 +37,7 @@ class Category extends Model
         //var_dump($results);
 
         $this->setData($results[0]);
+        Category::updateFile();
 
     }
 
@@ -68,6 +69,22 @@ class Category extends Model
         $sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory", array(
             ":idcategory" => $this->getidcategory() 
         ));
+
+        Category::updateFile();
+
+    }
+
+    public static function updateFile()
+    {
+        $categories = Category::listAll();
+
+        $html = [];
+
+        foreach ($categories as $row) {
+            array_push($html, '<li><a href="/categories/' . $row['idcategory'] . '">' . $row['descategory'] . '</a></li>');
+        }
+
+        file_put_contents($_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . "/views/categories-menu.html", implode('', $html));
 
     }
 
